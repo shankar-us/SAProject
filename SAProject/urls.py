44719b8e22171a706c3048a8b26415bdfca0bf31
shankar-us <shankar_us2000@yahoo.com>
2017-05-14 21:30:31 +0530

    Added bootstrap
    Added bootstrap

--- SAProject/urls.py
+++ SAProject/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from SAMain import views
 
-urlpatterns = [
+
+urlpatterns = {
     # Examples:
     # url(r'^$', 'SAProject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'^$', views.home)
+}
